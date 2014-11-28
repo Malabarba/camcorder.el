e609decaf825c9d7b01139c1b0656691d1fb0546
Artur Malabarba <bruce.connor.am@gmail.com>
2014-11-28 23:33:26 +0000

    Doc in the file

--- screencast.el
+++ screencast.el
@@ -23,8 +23,54 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; 
+;;
+;;   Tool for capturing screencasts directly from Emacs.
+;;
+;;   • To use it, simply call `M-x screencast-record'.
+;;   • A new smaller frame will popup and recording starts.
+;;   • When you're finished, hit `F12' and wait for the conversion to
+;;     finish.
+;;
+;;   Screencasts can be generated in any format understood by
+;;   `imagemagick''s `convert' command. You can even pause the recording
+;;   with `F11'!
+;;
+;;   If you want to record without a popup frame, use `M-x
+;;   screencast-mode'.
+;;
+;; Dependencies
+;; ────────────
+;;
+;;   `screencast.el' uses the [*Names*] package, so if you're installing
+;;   manually you need to install that too.
+;;
+;;   For the recording, `screencast.el' uses the following linux utilities.
+;;   If you have these, it should work out of the box. If you use something
+;;   else, you should still be able to configure `screencast.el' work.
+;;
+;;   • recordmydesktop
+;;   • mplayer
+;;   • imagemagick
+;;
+;;   Do you know of a way to make it work with less dependencies? *Open an
+;;   issue and let me know!*
+;;
+;;
+;;   [*Names*] https://github.com/Bruce-Connor/names/
+;;
+;; Troubleshooting
+;; ───────────────
+;;
+;;   On my machine, I noticed that the window-id Emacs reported
+;;   ┌────
+;;   │ (format "%x"
+;;   │   (string-to-number
+;;   │    (frame-parameter (selected-frame) 'window-id)))
+;;   └────
+;;   differed from the id that the WM reported with the `wminfo' utility. I
+;;   added the variable `screencast-window-id-offset' to correct that. The
+;;   default value is -4, but you might need to increase or decrease that
+;;   to make those two numbers match.
 
 ;;; Code:
 
